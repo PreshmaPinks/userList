3211c665d372b0a6536fcaa85c2402b7d0bc807a
PinksPreshma <pinkypreshma@gmail.com>
2021-02-23 12:33:21 +0530

    code and css addded

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData]=useState(null);
+  useEffect(()=>{
+    fetch("https://reqres.in/api/users")
+    .then(response=>response.json())
+    .then(response=>{
+      console.log(response.data)
+      setData(response.data)
+      console.log(response.data)
+    })
+  },[]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+    <input class="inputbox" type="text"/>
+    <button class="searchbutton">Search</button>
     </div>
+    <div>
+   
+    <table >
+   
+      {
+        data && data.map((item)=>{
+          return (
+            <>
+              
+            <tr> 
+              <td class="tddiv"> 
+              <div class="imagediv"><img src={item.avatar}></img></div>
+              </td>
+            
+            <td>
+              <div>Id:{item.id}</div>
+              <div>Email:{item.email}</div>
+              <div>FirstName:{item.first_name}   LastName:{item.last_name}</div>
+              </td>
+              </tr>
+                </>
+              
+              );                   
+        })
+      }
+      
+      </table>
+    </div>
+    </>
   );
 }
 
